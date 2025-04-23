4aa94e1f2c1e0f4173a28fa6f9ebf7d49f7de529
RaymonIdle <variluoxx@gmail.com>
2025-04-22 23:51:57 -0700

    Items class added

--- src/main/java/net/raymond/tutorialmod/TutorialMod.java
+++ src/main/java/net/raymond/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.raymond.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.raymond.tutorialmod.block.ModBlocks;
 import net.raymond.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize()
 	{
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/raymond/tutorialmod/item/ModItems.java
+++ src/main/java/net/raymond/tutorialmod/item/ModItems.java
@@ -12,8 +12,13 @@ public class ModItems {
 
     //ADDING ITEMS
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
     public static final Item ALEXANDRITE = registerItem("alexandrite", new Item(new Item.Settings()));
+    public static final Item RAW_ALEXANDRITE = registerItem("raw_alexandrite", new Item(new Item.Settings()));
+
     public static final Item BISMUTH = registerItem("bismuth", new Item(new Item.Settings()));
+    public static final Item RAW_BISMUTH = registerItem("raw_bismuth", new Item(new Item.Settings()));
     //Helper method
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -22,7 +27,12 @@ public class ModItems {
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(PINK_GARNET)
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+            entries.add(ALEXANDRITE);
+            entries.add(RAW_ALEXANDRITE);
+            entries.add(BISMUTH);
+            entries.add(RAW_BISMUTH);
         });
     }
 }
