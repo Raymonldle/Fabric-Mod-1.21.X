cb8b4bb3623507d53ae96cb319ca83b001177b18
RaymonIdle <variluoxx@gmail.com>
2025-04-22 19:17:44 -0700

    Items class added

--- src/main/java/net/raymond/tutorialmod/TutorialMod.java
+++ src/main/java/net/raymond/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.raymond.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.raymond.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize()
 	{
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/raymond/tutorialmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.raymond.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.raymond.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    //ADDING ITEMS
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item ALEXANDRITE = registerItem("alexandrite", new Item(new Item.Settings()));
+    public static final Item BISMUTH = registerItem("bismuth", new Item(new Item.Settings()));
+    //Helper method
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
+}
